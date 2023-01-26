9d3606da187d9b08bb2057583d165ca3c518d712
Minahal <minahal@techlo9.com>
2023-01-27 00:22:04 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import Home from './components/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
 
-      <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<Home />} />
-          <Route index element={<Home />} />
-
-        </Routes>
-      </BrowserRouter>
+      <Home />
 
     </div>
   );
