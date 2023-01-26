7c334e325db97db9357607f1a60fb5fb1d5d9d16
Minahal <minahal@techlo9.com>
2023-01-26 12:41:11 +0500

    add drop down

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import twitter from '../assets/twitter.png';
 import discord from '../assets/discord.png';
 import telegram from '../assets/telegram.png';
+import logo from '../assets/logo.png';
 
 
 
@@ -9,13 +10,11 @@ export default function Footer() {
   return (
     <div className="bg-blue-100">
       <footer data-aos="zoom-in" className='container text-white-100 mx-auto lg:px-12 font-poppins px-5'>
-        <div className="grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 items-baseline place-items-start gap-12 font-poppins py-8">
-          <div className="md:col-span-2 col-span-1">
+        <div className="grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 items-center place-items-start gap-12 font-poppins py-8">
+          <div className="md:col-span-2 col-span-1 flex items-center gap-3">
 
-            <a href="/" className="logo lg:my-12">
-              <h2 className='color-gradient md:text-5xl text-3xl font-orbitron items-center font-semibold'>
-                Meta Genesis
-              </h2>
+            <a href="/" className="lg:my-12">
+              <img src={logo} alt="Logo" />
             </a>
             <p className='py-5 leading-loose text-white-50'>Lets Explore and create your experience with<br></br>
               Metaverse</p>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom";
-
 
 
 export default function Menu() {
 
   const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+
 
   return (
 
@@ -20,10 +20,18 @@ export default function Menu() {
         <nav>
           <ul className="text-md flex justify-center gap-8 items-center">
             <li><a href="/" className="text-zinc-100">Home</a></li>
-            <li><a href="#nft" className="hover:text-seagreen-100 cursor-pointer">NFT</a></li>
-            <li><a href="#tokenomics" className="hover:text-seagreen-100 cursor-pointer">Tokenomics</a></li>
-            <li><a href="#roadmap" className="hover:text-seagreen-100 cursor-pointer">Roadmap</a></li>
-            <li><a href="#about" className="hover:text-seagreen-100 cursor-pointer">Ecosystem</a></li>
+            <li><a href="#nft" className=" cursor-pointer">NFT</a></li>
+            <li><a href="#tokenomics" className="cursor-pointer">Tokenomics</a></li>
+            <li><a href="#roadmap" className="cursor-pointer">Roadmap</a></li>
+            <li><a href="#about" className="cursor-pointer">Ecosystem</a></li>
+            <li><button onClick={() => setShow2(!show2)} className="cursor-pointer flex flex-col">DApp</button>
+              {
+                show2 ?
+                  <span className="cursor-pointer flex flex-col gap-3 p-2 mt-2 absolute bg-blue-100">
+                    <a href="/">Mint-NFT</a>
+                    <a href="/">Stake Token</a>
+                  </span> : null}
+            </li>
           </ul>
         </nav>
       </div>
@@ -44,31 +52,39 @@ export default function Menu() {
         </button>
       </div>
 
-      {show ? <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-3/4 overflow-y-auto text-center font-orbitron font-semibold bg-gradient-to-tr from-purple-100 to-purple-200 z-20" style={{ left: "0" }}>
+      {
+        show ? <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-3/4 overflow-y-auto text-center font-orbitron font-semibold bg-gradient-to-tr from-purple-100 to-purple-200 z-20" style={{ left: "0" }}>
 
-        <div className="mt-3 px-3 mb-12">
-          <a href="/" className="text-white-100">
-            <h2 className='uppercase text-2xl font-orbitron font-semibold'>
-              Meta Genesis
-            </h2>
-          </a>
-        </div>
-        <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md transition duration-500 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
-          <a href="/" className="text-lg ml-4 hover:text-purple-100 font-semibold">Home</a>
-        </button>
-        <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
-          <a href="#nft" className="text-lg ml-4 hover:text-purple-100 font-semibold">NFT 's</a>
-        </button>
-        <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
-          <a href="#tokenomics" className="text-lg ml-4 hover:text-purple-100 font-semibold">Tokenomics</a>
-        </button>
-        <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
-          <a href="#roadmap" className="text-lg ml-4 hover:text-purple-100 font-semibold">Roadmap</a>
-        </button>
-        <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
-          <a href="#about" className="text-lg ml-4 hover:text-purple-100 font-semibold">About Us</a>
-        </button>
-      </div> : null
+          <div className="mt-3 px-3 mb-12">
+            <a href="/" className="text-white-100">
+              <h2 className='uppercase text-2xl font-orbitron font-semibold'>
+                Meta Genesis
+              </h2>
+            </a>
+          </div>
+          <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md transition duration-500 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
+            <a href="/" className="text-lg ml-4 hover:text-purple-100 font-semibold">Home</a>
+          </button>
+          <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
+            <a href="#nft" className="text-lg ml-4 hover:text-purple-100 font-semibold">NFT 's</a>
+          </button>
+          <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
+            <a href="#tokenomics" className="text-lg ml-4 hover:text-purple-100 font-semibold">Tokenomics</a>
+          </button>
+          <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
+            <a href="#roadmap" className="text-lg ml-4 hover:text-purple-100 font-semibold">Roadmap</a>
+          </button>
+          <button onClick={() => setShow(!show)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100  text-white-100">
+            <a href="#about" className="text-lg ml-4 hover:text-purple-100 font-semibold">About Us</a>
+          </button>
+          <button onClick={() => setShow2(!show2)} className="w-full py-2.5 mt-3 flex items-center rounded-md duration-300 cursor-pointer hover:bg-white-100 hover:text-purple-100 pl-4 text-white-100">DApp</button>
+          {
+            show2 ?
+              <span className="cursor-pointer flex flex-col gap-3 p-2 mt-2 absolute text-white-100 text-left ">
+                <a href="/">Mint-NFT</a>
+                <a href="/">Stake Token</a>
+              </span> : null}
+        </div> : null
       }
 
 
